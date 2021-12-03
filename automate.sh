0cce0f999206dbe2ccc3172d87f2ff2f91979d78
swen-cloud-team-8 <95313078+swen-cloud-team-8@users.noreply.github.com>
2021-12-02 21:18:19 -0500

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -6,5 +6,7 @@ terraform init
 terraform apply
 chmod u+x test.sh
 sed -i -e 's/\r$//' test.sh
-read -p "Press enter to continue"
+read -p "Press enter to run the program"
 ./test.sh
+read -p "Press enter to destroy resources"
+terraform destroy
